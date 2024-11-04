3e415af1121782cae5d9a031d7d0ebd1f981a034
Ingoo Kang <47412229+kig2929kig@users.noreply.github.com>
2024-11-04 13:49:14 +0900

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,15 @@
+import sys
+
+from PySide6.QtWidgets import (
+    QApplication,
+    QWidget,
+    )
+
+app = QApplication(sys.argv)
+#print (sys.argv)
+
+window = QWidget()
+window.show()
+
+app.exec()
+
